152c98c5e1c3680cf089106c7e97dd63b1f9713a
clarisas <clarisasavio@gmail.com>
2018-09-03 11:41:52 -0300

    add Shot

---
+++ Source/UAO Shooter/Assets/Scripts/ShotBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotBehaviour : MonoBehaviour {
+	
+	public float Speed;
+	private Rigidbody rb;
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		rb.velocity = transform.forward * Speed;
+	}
+}
