77d90b2f1cc2a1023d1decbd268839de7b18dabf
Andres Virga <andvirga@gmail.com>
2018-09-03 11:52:21 -0300

    Renzo: Added particles background (stars)

---
+++ Source/UAO Shooter/Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+
+    public float speed = 0.05F;
+    private Renderer renderer;
+
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+
+        float offset = Time.time * speed;
+        renderer.material.SetTextureOffset("_MainTex", new Vector2(0, offset));
+    }
+}
