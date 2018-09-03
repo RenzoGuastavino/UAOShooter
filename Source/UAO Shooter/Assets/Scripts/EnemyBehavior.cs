4e44bf1a1a00bec15f32e4bad4c99399f3c39537
Andres Virga <andvirga@gmail.com>
2018-09-03 11:19:16 -0300

    Added waves of Enemies (Enemy Generator)

---
+++ Source/UAO Shooter/Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    //--Enemy's speed
+    public float speed;
+
+	// Use this for initialization
+	void Start ()
+    {
+        var rigidBody = GetComponent<Rigidbody>();
+        rigidBody.velocity = -transform.forward * speed;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
---
+++ Source/UAO Shooter/Assets/Scripts/TimeDestroyer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeDestroyer : MonoBehaviour
+{
+    //--Object lifeTime
+    public float lifeTime;
+
+    // Use this for initialization
+    void Start()
+    {
+        Destroy(this.gameObject, lifeTime);
+    }
+}
