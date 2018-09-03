00e183eaa7af4b53f30678cf336bc8557a6c405e
Andres Virga <andvirga@gmail.com>
2018-09-03 14:34:53 -0300

    Added Initial Screen (Starting Scene)

---
+++ Source/UAO Shooter/Assets/Scripts/StartingScene.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartingScene : MonoBehaviour
+{
+    public GameObject logo;
+    public Vector3 logoLimits;
+
+    // Use this for initialization
+    void Start()
+    {
+        var logoPosition = new Vector3
+        {
+            x = 0,
+            y = 0,
+            z = logoLimits.z
+        };
+        var logoRotation = Quaternion.identity;
+
+        //Instantiate(logo, logoPosition, logoRotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            Debug.Log("Enter here");
+            SceneManager.LoadScene("MainScene");
+        }
+    }
+}
